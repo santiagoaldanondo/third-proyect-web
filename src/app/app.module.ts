fe9935aea3a2b5c3fac90e4d78c32af1f39e63f5
santiagoaldanondo <santiagoaldanondo@gmail.com>
2017-09-28 19:05:38 +0200

    start with home, register and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,25 @@ import { ApolloModule } from 'apollo-angular';
 import { client } from './client';
 
 import { AppComponent } from './app.component';
-import { SignupComponent } from './signup/signup.component';
 import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'signup', pathMatch: 'full' },
-  { path: 'signup', component: SignupComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'users', component: UsersComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
 ]
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent,
     UsersComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
