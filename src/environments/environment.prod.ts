c0623bab36e706c08ace3ff3897ea382b1e93544
santiagoaldanondo <santiagoaldanondo@gmail.com>
2017-10-17 01:24:35 +0200

    fixes for ws in nginx

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'http://citayfactura.es/api/graphql',
-  wsUrl: 'ws://localhost:3000/api/subscriptions'
+  wsUrl: 'ws://citayfactura.es/api/subscriptions'
 };
